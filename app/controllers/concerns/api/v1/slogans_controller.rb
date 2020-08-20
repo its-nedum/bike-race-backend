cd0a5fcc2cd29cfff5bd52352cd468a2774fcc58
its-nedum <de.magnificent1@gmail.com>
2020-08-20 15:09:49 +0100

    chores(slogan controllers completed)

--- app/controllers/concerns/api/v1/slogans_controller.rb
+++ app/controllers/concerns/api/v1/slogans_controller.rb
@@ -9,6 +9,64 @@ module Api
                     data: slogans},
                     status: :ok
             end
+
+            def show
+                slogan = Slogan.find(params[:id])
+                render json: {
+                    status: 'Success',
+                    message: 'Loaded slogan',
+                    data: slogan},
+                    status: :ok
+            end
+
+            def create
+                slogan = Slogan.new(slogan_params)
+                if slogan.save
+                    render json: {
+                        status: 'Success',
+                        message: 'Saved slogan',
+                        data: slogan},
+                        status: :ok
+                else 
+                    render json: {
+                        status: 'Success',
+                        message: 'Slogan not saved',
+                        data: slogan.errors},
+                        status: :unprocessable_entity
+                end
+            end
+
+            def destroy
+                slogan = Slogan.find(params[:id])
+                slogan.destroy
+                render json: {
+                    status: 'Success',
+                    message: 'Slogan deleted',
+                    data: slogan},
+                    status: :ok
+            end
+
+            def update
+                slogan = Slogan.find(params[:id])
+                if slogan.update_attributes(slogan_params)
+                    render json: {
+                        status: 'Success',
+                        message: 'Slogan updated',
+                        data: slogan},
+                        status: :ok
+                    else 
+                        render json: {
+                            status: 'Success',
+                            message: 'Slogan not updated',
+                            data: slogan},
+                            status: :unprocessable_entity
+
+
+            private
+
+            def slogan_params
+                params.permit(:firstname, :lastname, :email, :slogan)
+            end
         end
     end
 end
