892ddf807d873cd5f70a15b93a66dfeaca654498
its-nedum <de.magnificent1@gmail.com>
2020-08-25 16:04:12 +0100

    chores(first test passed)

--- Gemfile
+++ Gemfile
@@ -28,9 +28,7 @@ gem 'rack-cors', :require => 'rack/cors'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails', '~> 4.0.0'
   gem 'faker'
-  gem 'factory_bot_rails'
 end
 
 group :development do
--- test/controllers/api/v1/slogans_controller_test.rb
+++ test/controllers/api/v1/slogans_controller_test.rb
@@ -2,6 +2,22 @@
 
 require 'test_helper'
 
-class SlogansControllerTest < ActionDispatch::IntegrationTest
+module Api
+    module V1
+        class SlogansControllerTest < ActionDispatch::IntegrationTest
 
+            test 'user submit slogan' do
+               slogan_params = {
+                firstname: Faker::Name.first_name,
+                lastname: Faker::Name.last_name,
+                email: Faker::Internet.email,
+                slogan: Faker::Lorem.paragraph_by_chars(number: 50, supplemental: false),
+               }
+
+               post '/api/v1/slogans', params: slogan_params 
+               assert_response :success
+            end
+
+        end
+    end
 end
