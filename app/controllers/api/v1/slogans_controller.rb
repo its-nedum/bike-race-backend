6e9d518699046b840bce490b23792b1f3109bcb6
its-nedum <de.magnificent1@gmail.com>
2020-10-26 16:01:17 +0100

    features(wrote test for API endpoint)

--- app/controllers/api/v1/slogans_controller.rb
+++ app/controllers/api/v1/slogans_controller.rb
@@ -1,15 +1,55 @@
 module Api
     module V1
         class SlogansController < ApplicationController
+            
+            # This method create a user slogan entry and also prevent multiple submission
+            # by checking the user's email with those in the database
+            def create
+                slogan = Slogan.new(slogan_params)
+                #check if the user with email address has submitted
+                if Slogan.exists?(:email => slogan.email)
+                    render json: {
+                        status: 'Warning',
+                        message: 'Record already exists',
+                        data: slogan,
+                        code: 208
+                    }
+                else
+                    if slogan.save
+                        render json: {
+                            status: 'Success',
+                            message: 'Submission saved',
+                            data: slogan,
+                            code: 201,
+                        }
+                    else 
+                        render json: {
+                            status: 'Error',
+                            message: 'Submission not saved',
+                            data: slogan.errors,
+                            code: 422,
+                        }
+                    end
+                end
+            end
+
+            # This method returns all the submitted slogans
             def index
                 slogans = Slogan.order('id ASC');
                 render json: {
                     status: 'Success',
                     message: 'Loaded slogans',
-                    data: slogans},
-                    status: :ok
+                    data: slogans,
+                    code: 200,
+                }
             end
 
+
+
+            # NOTE: The following methods below are extra CRUD endpoints I created for practice
+            # they are not implemented
+
+            # This method returns a single slogan
             def show
                 slogan = Slogan.find(params[:id])
                 render json: {
@@ -19,32 +59,7 @@ module Api
                     status: :ok
             end
 
-            def create
-                slogan = Slogan.new(slogan_params)
-                #check if the user with email address has submitted
-                if Slogan.exists?(:email => slogan.email)
-                    render json: {
-                        status: 'Warning',
-                        message: 'Record already exists',
-                        data: slogan},
-                        status: :ok
-                else
-                    if slogan.save
-                        render json: {
-                            status: 'Success',
-                            message: 'Submission saved',
-                            data: slogan},
-                            status: :ok
-                    else 
-                        render json: {
-                            status: 'Error',
-                            message: 'Submission not saved',
-                            data: slogan.errors},
-                            status: :unprocessable_entity
-                    end
-                end
-            end
-
+            # This method deletes a user slogan submission
             def destroy
                 slogan = Slogan.find(params[:id])
                 slogan.destroy
@@ -55,6 +70,7 @@ module Api
                     status: :ok
             end
 
+            # This method allow for updating a submitted slogan
             def update
                 slogan = Slogan.find(params[:id])
                 if slogan.update_attributes(slogan_params)
--- test/controllers/api/v1/slogans_controller_test.rb
+++ test/controllers/api/v1/slogans_controller_test.rb
@@ -6,17 +6,44 @@ module Api
     module V1
         class SlogansControllerTest < ActionDispatch::IntegrationTest
 
-            test 'user submit slogan' do
+            test 'should return all submitted slogan' do
+                # make a get a request
+                get api_v1_slogans_path
+
+                # the endpoint response must include data object
+                assert_includes(@response.body, 'data')
+
+                # get the status code from the response
+               code = JSON.parse(@response.body)['code']
+
+                # ensure that the status code is equal to 201 :Created
+               assert_equal(code, 200)
+               
+               assert_response :success
+            end
+
+            test 'should create a slogan when all params are provided' do
+                # prepare the slogan parameter
                slogan_params = {
                 firstname: Faker::Name.first_name,
                 lastname: Faker::Name.last_name,
                 email: Faker::Internet.email,
                 slogan: Faker::Lorem.paragraph_by_chars(number: 50, supplemental: false),
                }
+                # make post request   
+               post api_v1_slogans_path, params: slogan_params
 
-               post '/api/v1/slogans', params: slogan_params 
+                # the endpoint response must include data object
+                assert_includes(@response.body, 'data')
+
+                # get the status code from the response
+               code = JSON.parse(@response.body)['code']
+                # status code must be equal to 201 :Created
+               assert_equal(code, 201)
+
                assert_response :success
             end
+
 
         end
     end
